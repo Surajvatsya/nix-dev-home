9c79442530d07547cc35cc57d4ac0473c292a8f3
suraj.kumar1 <suraj.kumar1@juspay.in>
2023-05-10 19:56:10 +0530

    Add shell aliases

--- home.nix
+++ home.nix
@@ -21,6 +21,31 @@
 
     # For macOS's default shell.
     zsh = {
+
+      shellAliases = {
+        gl = "git pull";
+        code-backend="code /Users/suraj.kumar1/nammayatri/Backend";
+        code-kernel="code /Users/suraj.kumar1/shared-kernel";
+        code-custom="code ~/.oh-my-zsh/custom/";
+        code-config="code /Users/suraj.kumar1/nix-config";
+        svc=", run-svc";
+        qsvc="nix run .#arion -- up --remove-orphans -d";
+        sac="nix run .#arion -- down --remove-orphans";
+        pgadmin="nix run .#arion -- up --remove-orphans pg-admin";
+        qpgadmin="nix run .#arion -- up --remove-orphans -d pg-admin";
+        rms=", run-mobility-stack-dev";
+        kill-server="sh /Users/suraj.kumar1/.oh-my-zsh/custom/portkill.sh";
+        gsta="git stash";
+        gstaa="git stash apply";
+        gb="git branch";
+        gco="git checkout";
+        gcb="git checkout -b";
+        backend="cd /Users/suraj.kumar1/nammayatri/Backend";
+        kernel="cd /Users/suraj.kumar1/shared-kernel";
+        ".."="cd ..";
+        server-cold-start="sh /Users/suraj.kumar1/.oh-my-zsh/custom/server-start.sh";
+      };
+
       enable = true;
       envExtra = ''
         # Make Nix and home-manager installed things available in PATH.
